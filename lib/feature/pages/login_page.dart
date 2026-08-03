fbb3682c0b9ee12af76c19de2978ebecf5a28b52
FAHIMSHAKIL102 <fahimshakil102@gmail.com>
2026-08-03 22:55:58 +0600

    feat: mytextformfield

--- lib/feature/pages/login_page.dart
+++ lib/feature/pages/login_page.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_minimal_social_media_app/widget/my_textformfield.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final emailController = TextEditingController();
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.surface,
       body: Center(
@@ -20,9 +22,14 @@ class LoginPage extends StatelessWidget {
             SizedBox(height: 25),
 
             // app name
-            Text('Social Media App',style: TextStyle(fontSize: 25),),
+            Text('Social Media App', style: TextStyle(fontSize: 25)),
+            SizedBox(height: 25),
             // email textfield
-
+            MyTextformfield(
+              hintText: 'hintText',
+              obscureText: false,
+              controller: emailController,
+            ),
             // password textfield
 
             // forgot password
---
+++ lib/widget/my_textformfield.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class MyTextformfield extends StatelessWidget {
+  final String hintText;
+  final bool obscureText;
+  final TextEditingController controller;
+  const MyTextformfield({
+    super.key,
+    required this.hintText,
+    required this.obscureText,
+    required this.controller,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: controller,
+      obscureText: true,
+      decoration: InputDecoration(
+        hintText: hintText,
+        border: OutlineInputBorder(),
+      ),
+    );
+  }
+}
