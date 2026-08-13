8c8e28e53423d05d1e005267067273841dfb4e9d
FAHIMSHAKIL102 <fahimshakil102@gmail.com>
2026-08-13 15:30:16 +0600

    feat: profile and user page

--- lib/feature/pages/home_page.dart
+++ lib/feature/pages/home_page.dart
@@ -1,21 +1,20 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_minimal_social_media_app/widget/my_drawer.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
-  // logout user
-  void logout() {
-    FirebaseAuth.instance.signOut();
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Home'),
-        actions: [IconButton(onPressed: logout, icon: Icon(Icons.logout))],
+        backgroundColor:Theme.of(context).colorScheme.inversePrimary ,
+      
       ),
+      drawer: MyDrawer(),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_minimal_social_media_app/auth/auth_page.dart';
+import 'package:flutter_minimal_social_media_app/auth/login_or_register.dart';
+import 'package:flutter_minimal_social_media_app/feature/pages/home_page.dart';
+import 'package:flutter_minimal_social_media_app/feature/pages/login_page.dart';
+import 'package:flutter_minimal_social_media_app/feature/pages/profile_page.dart';
+import 'package:flutter_minimal_social_media_app/feature/pages/register_page.dart';
+import 'package:flutter_minimal_social_media_app/feature/pages/user_page.dart';
 import 'package:flutter_minimal_social_media_app/feature/theme/dark_mode.dart';
 import 'package:flutter_minimal_social_media_app/feature/theme/light_mode.dart';
 import 'package:flutter_minimal_social_media_app/firebase_options.dart';
@@ -21,6 +27,14 @@ class MyApp extends StatelessWidget {
       home: AuthPage(),
       theme: lightMode,
       darkTheme: darkMode,
+      routes: {
+        '/login_page':(context) => LoginPage(),
+        '/register_page': (context) => RegisterPage(),
+        '/login_register_page': (context) => LoginOrRegister(),
+        '/home_page':(context) => HomePage(),
+        '/profile_page':(context) => ProfilePage(),
+        '/user_page': (context) => UserPage(),
+      },
     );
   }
 }
